e17c26e0359652a08a48d7d0c282229d7fecc802
Kalaimagal-eng <140145316+Kalaimagal-eng@users.noreply.github.com>
2023-09-06 02:37:50 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div>
+            <h1>Welcom to Internsync</h1>
+            <p>Empowering Your Summer Internship Experience. InternSync is your dedicated hub for a seamless and 
+       enriching summer internship experience within our organization. With carefully crafted features, InternSync
+       simplifies communication, scheduling and collaboration to ensure you make the most of your internship
+       Together, we'll make your internship journey not only productive but also enjoyable and impactful.
+       Begin Your internship..
+            </p>
+        </div>
+    );
 }
+
+  
+
 
 export default App
